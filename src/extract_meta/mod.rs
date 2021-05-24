fc73d9bcd84a75bcbb23e0430df0fa72436ae0dc
Johannes Vollmer <32042925+johannesvollmer@users.noreply.github.com>
2021-05-24 14:37:03 +0200

    insert basic working code to verify that this architecture works

--- src/extract_meta/mod.rs
+++ src/extract_meta/mod.rs
@@ -1,5 +1,11 @@
 
+pub fn main(mut args: impl Iterator<Item=String>) {
+    let path = args.next().expect("no path specified"); // TODO better error handling!
+    println!("loading image from path {}", path);
 
-fn main(){
-    println!("hello world")
+    let meta = exr::meta::MetaData::read_from_file(path, false).unwrap();
+
+    // print the stuff
+    // TODO make it nice (I suspect Narann already crafted some code for this? haha)
+    println!("{:#?}", meta);
 }
