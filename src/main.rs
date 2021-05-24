bdfc8a81b51b86301bb4b5eb5e34054536eb1451
Johannes Vollmer <32042925+johannesvollmer@users.noreply.github.com>
2021-05-24 14:14:57 +0200

    propose single-app architecture

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod extract_meta;
+mod edit_meta;
+mod compression_stats;
+
+fn main(){
+    // todo:
+    //    - parse first env argument (ex. "extract-meta" in "exr-tools extract-meta x y z")
+    //    - match argument and call the corresponding module main ("extract_meta::main()")
+}
